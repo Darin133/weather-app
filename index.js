093e5370b320e544d3a79c7181ae2c4fc1e9283b
Darin133 <darinchik6766@gmail.com>
2022-08-30 12:43:33 +0300

    add apiurl forecast

--- index.js
+++ index.js
@@ -41,6 +41,12 @@ function searchForCity(event) {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchForCity);
 
+function getForecast(coordinates) {
+  let apiKey = "8bf13fea7fc7416d4df009d0ae146aff";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showWeather(response) {
   let city = document.querySelector("h1");
   city.innerHTML = response.data.name;
@@ -58,6 +64,8 @@ function showWeather(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function findCurrentPlace(position) {
@@ -101,7 +109,8 @@ Fbutton.addEventListener("click", showFahrenheit);
 let Cbutton = document.querySelector("#Celsius");
 Cbutton.addEventListener("click", showCelsius);
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<ul>`;
   let days = [
@@ -121,5 +130,4 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
-displayForecast();
 search("New York");
